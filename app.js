28cbb15043ff2800f22d1a1580fc92ec6a868f0e
Chetan Kurvatti <klchetan@gmail.com>
2015-12-13 19:39:07 -0500

    Node Samples

---
+++ app.js
@@ -0,0 +1,39 @@
+function User() {
+    this.name = "";
+    this.life = 60;
+    this.giveLife = function(targetPlayerLife) {
+        console.log("Inside giveLife function life:" + targetPlayerLife.life);
+        targetPlayerLife.life += 1;
+    }
+}
+
+var Chetan = new User();
+Chetan.name = "Chetan";
+
+console.log("Logging Name:" + Chetan.name);
+console.log("Logging life:" + Chetan.life);
+
+var test = new User();
+test.name = "Raghu";
+Chetan.giveLife(test);
+
+console.log("Chetan's life: " + Chetan.life)
+console.log("Logging Name:" + test.name);
+console.log("Logging life:" + test.life);
+
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+console.log("Server is running");
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
